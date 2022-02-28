d71cb912f93992a6fc7e71b1cb8d4c3396edaafa
kelsileo25 <94098900+kelsileo25@users.noreply.github.com>
2022-02-28 11:27:36 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+name = 'Covalence';
+var name;
+console.log(name);
+
+setName();
+
+function setName() {
+    var name = 'Covalence';
+    console.log(name);
+}
+
+
+
+
+
+console.log('some t');
+let avg = findingAvg(6, 2);
+console.log('some t', avg);
+
+function findingAvg(a, b) {
+    console.log('some t');
+    var answer = (a + b) / 2;
+    return answer;
+}
+
+findingAvg();
+
+let fruits = ['apples', 'grapes', 'kiwi'];
+
+
+function printFruits() {
+    favFruit = fruits[2];
+    console.log(fruits[0]);
+}
+
+function printFavFruit() {
+    console.log(favFruit);
+    let leastFav = fruits[0];
+    console.log(leastFav);
+
+}
+
+printFruits();
+printFavFruit();
+
+Somefunc();
+
+function Somefunc() {
+    console.log('Hello Markel');
+}
+
+
+
+toSoon();
+let text = toSoon;
+
+function toSoon() {
+
+    alert('My Love!');
+}
